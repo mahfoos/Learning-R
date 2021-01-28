717315cdac35f805854d2ddda4ae11bf2a06ec1a
Mahfoos <umahfoosahamed@gmail.com>
2021-01-28 20:54:17 +0530

    commit

--- Data type and strucures.R
+++ Data type and strucures.R
@@ -58,3 +58,35 @@ n2 <- matrix(c("0","B",
                 nrow = 2,
                 byrow = T)
 n2
+
+
+# Array #############################
+
+# Give data, then dimension (rows, columns, tables)
+a1 <- array(c(1:24),c(4,3,2))
+a1
+
+
+# Data Frame ########################
+
+# Can combine vectors of the same length
+
+vNumeric <- c(1,2,3)
+vCharacter <- c("o","b","c")
+vLogical <- c(T,F,F)
+
+df1 <- cbind(vNumeric,vCharacter,vLogical)
+df1 # Coerces all values to most basic data type
+
+df2 <- as.data.frame(cbind(vNumeric,vCharacter,vLogical))
+df2 # Makes a data frame with three different data types
+
+
+# List ##################################
+
+o1 <- c(1,2,3)
+o2 <- c("a","b","c","d")
+o3 <- c(T,F,T,T,F)
+
+list1 <- list(o1,o2,o3)
+list1
