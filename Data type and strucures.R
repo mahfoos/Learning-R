d1f66bdce3606f2010890be9fbc0e4f29d2b2322
Mahfoos <umahfoosahamed@gmail.com>
2021-01-28 20:20:07 +0530

    commit

---
+++ Data type and strucures.R
@@ -0,0 +1,32 @@
+# Projecct: Learning R
+
+# DATA TYPES ######################
+
+# Numeric 
+
+n1 <- 15  # Double precision by default
+n1
+typeof(n1)
+
+n2 <-1.5
+n2
+typeof(n2)
+
+
+# Character 
+
+c1 <- "c"
+c1
+typeof(c1)
+
+c2 <- "a string of text"
+c2
+typeof(c2)
+
+
+# Logical
+
+l1 <- TRUE
+l1
+typeof(l1)
+
---
+++ Sample.R
@@ -0,0 +1,42 @@
+print("hello world")
+
+# BASIC COMMANDS ######################################
+
+2+2 # Basic math
+
+1:100 # Print numbers 1 to 100 across several lines
+
+# ASSINING VALUES #######################
+
+# Individuals values
+
+a <- 1            # Use <- and not =
+2 ->b             # Can go other way, but silly
+c <- d <- e <- 3  # Multiple assignments
+
+# Multiple values 
+x <- c(1,2,5,9)
+x
+
+# Sequences #############
+
+0:10               # 0 through 10
+10:0               # 10 through 0
+seq(10)            # 1 to 10
+seq(30,0, by = -3) # Count down by 3
+
+
+# MATH ###################
+
+(y <- c(5,1,0,10))
+x+y
+x*2
+2^6
+sqrt(60)  # print squre root of 60
+log(100)  # log of 100
+log10(100) 
+
+# Delete and Remove ###########
+
+rm(a) # Remove the variable name 
+ls()  # Print the existing variable name
